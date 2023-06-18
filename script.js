70694260f39ea7145706ae7db693eeee43c36b06
linq <info@linq84.com>
2023-06-18 22:57:16 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function callPhoneNumber() {
+    window.location.href = "tel:YOUR_NUMBER_HERE";
+  }
+
+  // Call the function after 5 seconds (5000 milliseconds)
+  setTimeout(callPhoneNumber, 5000);
+
